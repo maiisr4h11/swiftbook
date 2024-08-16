ab12a774271cfd728d87f6225166b97ac682a18e
maiisr4h11 <maisarahibrahim2511@gmail.com>
2024-08-16 09:01:17 +0800

    FoodFest is edited

--- frontend/src/Event/FoodFest.js
+++ frontend/src/Event/FoodFest.js
@@ -32,18 +32,17 @@ function FoodFest() {
             <section className='event-list'>
                 <div className='container mt-5 mb-5'>
                     <div class="row" data-aos="fade-up">
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             <div class="card text-center">
                                 <div class="card-body">
                                     <img className='card-img-top' src='images/poster-1.png' alt=''></img>
                                     <h5 class="card-title text-center mt-2 fs-4">Street Food Festival</h5>
                                     <p class="card-text">Some text for item 1</p>
                                     <div className='btn btn-primary text-center'>View Details</div>
-                                </div>
-                                
+                                </div>     
                             </div>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             <div class="card text-center">
                                 <div class="card-body">
                                 <img className='card-img-top' src='images/poster-2.png'alt=''></img>
@@ -53,7 +52,17 @@ function FoodFest() {
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-3">
+                            <div class="card text-center">
+                                <div class="card-body">
+                                <img className='card-img-top' src='images/poster-2.png'alt=''></img>
+                                    <h5 class="card-title text-center mt-2 fs-4">Night Market</h5>
+                                    <p class="card-text">Some text for item 2</p>
+                                    <div className='btn btn-primary text-center'>View Details</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
                             <div class="card text-center">
                                 <div class="card-body">
                                 <img className='card-img-top' src='images/poster-3.png'alt=''></img>
