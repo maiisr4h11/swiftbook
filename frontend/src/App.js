c595fa7f94c1ece4128f0378fd00289234971faf
wankhairul11 <w.k.aiman1993@gmail.com>
2024-08-19 06:50:15 +0800

    files added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AboutUs from './About/AboutUs';
+import ContactUs from './Contact/ContactUs';
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
