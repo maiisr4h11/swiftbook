32149664d593a0a148b4435f1041ad0b29b9733a
amir <amirmuttaqinhub@gmail.com>
2024-08-19 15:17:59 +0800

    frontend/signup and login - v1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import './App.css'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,148 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Login.css'; // You can use the same CSS or create a new one for Signup
+import Logo from '../Assets/SwiftbookLogo.jpg';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+function Signup() {
+    const navigate = useNavigate();
+
+    AOS.init({
+        duration: 1200,
+        once: false,
+        mirror: false,
+    });
+
+    const [formData, setFormData] = useState({
+        fullName: '',
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+    const [errors, setErrors] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const validateForm = () => {
+        let formErrors = {};
+        if (!formData.fullName) formErrors.fullName = 'Full Name is required';
+        if (!formData.username) formErrors.username = 'Username is required';
+        if (!formData.email) formErrors.email = 'Email is required';
+        if (!formData.password) formErrors.password = 'Password is required';
+        if (formData.password !== formData.confirmPassword) {
+            formErrors.confirmPassword = 'Passwords do not match';
+        }
+        return formErrors;
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formErrors = validateForm();
+        if (Object.keys(formErrors).length === 0) {
+            console.log('Form Data:', formData);
+            setIsSubmitted(true);
+            // Redirect after successful signup
+            navigate('/', { state: { signupSuccess: true } });
+            alert("Signup Successful");
+        } else {
+            setErrors(formErrors);
+        }
+    };
+
+    return (
+        <div className='container d-flex justify-content-center align-items-center vh-100' data-aos="flip-left">
+            <div className="form-container signup-panel d-flex justify-content-evenly align-items-center p-5 shadow rounded bg-white w-100 vh-50">
+                <div className='image-container left object-fit'>
+                    <img src={Logo} alt="signup-image" />
+                    <p className="mt-3">
+                        Already have an account? <Link to="/Login" className="text-decoration-none">Log In</Link>
+                    </p>
+                </div>
+                <div className="d-flex flex-column justify-content-center align-items-center rounded shadow p-5 bg-light" data-aos="fade-down">
+                    <div className="mb-5">
+                        <h2 className='display-4 p-3'>Sign Up</h2>
+                    </div>
+                    {isSubmitted ? (
+                        <div className="success-message text-center">Sign up successful!</div>
+                    ) : (
+                        <form onSubmit={handleSubmit} className="w-100" data-aos="fade-up">
+                            <div className="mb-3">
+                                <label className="form-label">Full Name:</label>
+                                <input
+                                    type="text"
+                                    name="fullName"
+                                    value={formData.fullName}
+                                    onChange={handleChange}
+                                    className="form-control"
+                                    required
+                                />
+                                {errors.fullName && <span className="error text-danger">{errors.fullName}</span>}
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label">Username:</label>
+                                <input
+                                    type="text"
+                                    name="username"
+                                    value={formData.username}
+                                    onChange={handleChange}
+                                    className="form-control"
+                                    required
+                                />
+                                {errors.username && <span className="error text-danger">{errors.username}</span>}
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label">Email:</label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    className="form-control"
+                                    required
+                                />
+                                {errors.email && <span className="error text-danger">{errors.email}</span>}
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label">Password:</label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    value={formData.password}
+                                    onChange={handleChange}
+                                    className="form-control"
+                                    required
+                                />
+                                {errors.password && <span className="error text-danger">{errors.password}</span>}
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label">Confirm Password:</label>
+                                <input
+                                    type="password"
+                                    name="confirmPassword"
+                                    value={formData.confirmPassword}
+                                    onChange={handleChange}
+                                    className="form-control"
+                                    required
+                                />
+                                {errors.confirmPassword && <span className="error text-danger">{errors.confirmPassword}</span>}
+                            </div>
+                            <button type="submit" className="btn btn-primary w-100">Sign Up</button>
+                        </form>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Signup;
